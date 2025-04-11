f7166dd0361780b0ee34ba88c3c9370007a063ba
tevitamathias <mathias.tevita@icloud.com>
2025-04-10 21:51:24 -0700

    created method to check if target is a hit and created a function to reveal or hide the ships.

--- src/Main/Board.java
+++ src/Main/Board.java
@@ -57,4 +57,65 @@ public class Board {
         return true;
     }
 
+    public boolean attackHits(int row, int col){
+        //returns false if that area has already been hit
+        if(grid[row][col] == 'X' || grid[row][col] == '0'){
+            return false;
+        }
+        //if that location is a ship
+        if(grid[row][col] == 'S'){
+            //changes it to X to mark the location as a hit
+            grid[row][col] = 'X';
+            //loops throughthe ships locations to see if there is a ship there
+            for(Ship ship : ships){
+                if(isHit(ship, row, col)){
+                    ship.hit();
+                }
+            }
+            return true;
+        }
+        //if it doesnt hit replaces that location to 0 marking a miss
+        else{
+            grid[row][col] = '0';
+            return false;
+        }
+    }
+    //loops through all the ships to see if they have all been sunk
+    public boolean allShipsSunk(){
+        for(Ship ship: ships){
+            if(!ship.isSUnk()){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public void print(boolean revealShips){
+        System.out.print(" ");
+        for(int i = 0; i < gsize; i++){
+            System.out.print(i + " ");
+            for(int j = 0; j< gsize; j++){
+                char c = grid[i][j];
+                if(!revealShips && c == 'S'){
+                    System.out.print(c + " ");
+                }
+            }
+            System.out.println();
+        }
+    }
+    //checks if target corresponds to a ship location
+    public boolean isHit(Ship ship, int row, int col){
+        int r = ship.getRow();
+        int c = ship.getCol();
+        for(int i = 0; i < ship.getSize(); i++){
+            //if ship is horizontal uses left value if not uses right value
+            int checkRow = ship.isHorizontol() ? r : r  + i;
+            int checkCol = ship.isHorizontol()? c + i : c ;
+            if(checkRow == row && checkCol == col){
+                return true;
+            }
+        }
+        return false;
+    }
+
 }
